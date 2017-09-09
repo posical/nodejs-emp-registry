4e9a908bab04e7785c1ebc17e52dbd5ecfc8d27e
posical <gforcenova@gmail.com>
2017-09-09 00:43:14 +0000

    moving on

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require ('express'),
+  app = express(),
+//  port = process.env.PORT || 3000;
+  port = 3000;
+
+app.listen(port);
+
+console.log('Employee Registry RESTful API Server started on ' + port);
+
+
